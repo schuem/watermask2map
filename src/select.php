2a13415110f462b95767f71602e3d5b3e7b30fd1
schuem <mschuettig@mailbox.org>
2018-04-05 00:00:38 +0200

    using json objects

--- src/select.php
+++ src/select.php
@@ -1,31 +1,37 @@
 <?php
 require_once 'dbconnect.php';
 if (isset($_GET['id'])) {
-  $sql = "SELECT id_cogerh, ingestion_time, area, s.name FROM masks m " 
+  $sql = "SELECT ST_AsGeoJSON(geom) AS geom, id_cogerh, ingestion_time, area, s.name FROM masks m " 
           . "INNER JOIN source s on m.source_id = s.source_id WHERE id_cogerh = '".$_GET['id']."' "
           . "ORDER BY ingestion_time DESC";
   $res = pg_query($sql);
   $arr_data = pg_fetch_all($res);
+  $json = json_encode($arr_data, JSON_PRETTY_PRINT); 
+  echo $json;
 
-  echo "<table class='table table-bordered table-condensed'>";
-    echo "<caption> Data for Cogerh ID ".$arr_data[0]['id_cogerh']."</caption>";
-    // header
-    echo "<thead><tr>";
-      echo "<th>Ingestion Time</th>";
-      echo "<th>Area</th>";
-      echo "<th>Source</th>";
-    echo "</thead></tr>";
-    // content
-    echo "<tbody>";
-      foreach ($arr_data as $v) {
-        echo "<tr>";
-          echo "<td>" .$v['ingestion_time']. "</td>";
-          echo "<td>" .$v['area']. "</td>";
-          echo "<td>" .$v['name']. "</td>";
-        echo "</tr>";
-      }
-    echo "</tbody>";
-  echo "</table>";
+  //echo "json_encode($arr_data, JSON_PRETTY_PRINT)";
+
+  // echo "<table class='table table-bordered table-condensed'>";
+  //   echo "<caption> Data for Cogerh ID ".$arr_data[0]['id_cogerh']."</caption>";
+  //   // header
+  //   echo "<thead><tr>";
+  //     echo "<th>Ingestion Time</th>";
+  //     echo "<th>Area</th>";
+  //     echo "<th>Source</th>";
+  //   echo "</thead></tr>";
+  //   // content
+  //   echo "<tbody>";
+  //     foreach ($arr_data as $v) {
+  //       echo "<tr>";
+  //         echo "<td>" .$v['ingestion_time']. "</td>";
+  //         echo "<td>" .$v['area']. "</td>";
+  //         echo "<td>" .$v['name']. "</td>";
+  //       echo "</tr>";
+  //     }
+  //   echo "</tbody>";
+  // echo "</table>";
+
+
   //print_r(pg_fetch_all($res));
 }
 ?>
