24af142587790d84f8ffdd6ca033744dbd3ffefe
Martin Schuettig <mschuettig@mailbox.org>
2018-04-20 12:18:57 +0200

    using the panel again to show information

--- src/select.php
+++ src/select.php
@@ -1,19 +1,57 @@
 <?php
 require_once 'dbconnect.php';
 if (isset($_GET['id'])) {
-  $sql = "SELECT ST_AsGeoJSON(geom) AS geom, id_cogerh, ingestion_time, area, s.name FROM masks m " 
+  // $sql = "SELECT ST_AsGeoJSON(geom) AS geom, id_cogerh, ingestion_time, area, s.name FROM masks m " 
+  //         . "INNER JOIN source s on m.source_id = s.source_id WHERE id_cogerh = '".$_GET['id']."' "
+  //         . "ORDER BY ingestion_time DESC";
+  // $sql = "SELECT row_to_json(f) AS feature FROM "
+  //         . "(SELECT 'Feature' AS type, ST_AsGeoJSON(geom)::json As geometry, "
+  //         . "row_to_json (SELECT id_cogerh, ingestion_time, area, s.name FROM masks m "
+  //         . "INNER JOIN source s on m.source_id = s.source_id WHERE id_cogerh = '".$_GET['id']."' "
+  //         . "ORDER BY ingestion_time DESC) AS f";
+  $sql = "SELECT json_build_object(
+            'type',       'Feature',
+            'id',         id,
+            'geometry',   ST_AsGeoJSON(geom)::json,
+            'properties', json_build_object(
+                'feat_type',      ST_GeometryType(geom),
+                'id_cogerh',      id_cogerh,
+                'ingestion_time', ingestion_time,
+                'area',           area,
+                'source',         s.name
+             )
+          ) AS features FROM masks m "
           . "INNER JOIN source s on m.source_id = s.source_id WHERE id_cogerh = '".$_GET['id']."' "
           . "ORDER BY ingestion_time DESC";
   $res = pg_query($sql);
   $arr_data = pg_fetch_all($res);
-  $json = json_encode($arr_data, JSON_PRETTY_PRINT);
+  //print_r($arr_data);
+  // works with first $sql query to decode already encoded json,
+  // otherwise it will be encoded twice
+  $processed = array();
+  // foreach ($arr_data as &$d) {
+  //   $d['features'] = json_decode($d['features'], TRUE);
+  //   //print_r($d);
+  // //   //echo $d['features'];
+  // }
+  
+  foreach ($arr_data as $key => $value) {
+    //print_r(json_decode($d['features'], TRUE));
+    //echo empty($value['features']) ? 'leeres Features' : 'FEATURE!';
+    //echo nl2br("\n\n");
+    //$processed = $processed + json_decode($d['features'], TRUE);
+    $processed[$key] = json_decode($value['features'], TRUE);
+    //echo "is array:" . is_array(json_decode($d, TRUE));
+    //print_r($d);
+    //echo $d['features'];
+  }
+
+  //print_r($processed);
+  //echo count($processed);
+  $json = json_encode($processed);
   // write json file to disc and echo
-  # try fwrite!!!!!!
-  file_put_contents('data.json', $json);
-  //$fp = fopen('data.json', 'w');
-  //fwrite($fp, $json);
-  //fclose($fp);
-  echo json_encode($arr_data, JSON_PRETTY_PRINT);
+  //file_put_contents('reservoirsById.json', $json);
+  echo $json;
 
   //echo "json_encode($arr_data, JSON_PRETTY_PRINT)";
 
@@ -39,5 +77,8 @@ if (isset($_GET['id'])) {
 
 
   //print_r(pg_fetch_all($res));
+
+
+
 }
 ?>
