768b10946589bd7baf0303108f7ba37a506dbf77
Martin Schuettig <mschuettig@mailbox.org>
2018-05-09 16:49:18 +0200

    query doesn't return all rows

--- src/select.php
+++ src/select.php
@@ -15,20 +15,33 @@ if (isset($_GET['id'])) {
           ) AS features FROM masks m "
           . "INNER JOIN source s on m.source_id = s.source_id WHERE id_cogerh = '".$_GET['id']."' "
           . "ORDER BY ingestion_time DESC";
+  //$sql = "SELECT * FROM MASKS WHERE id_cogerh = '".$_GET['id']."' ";
   $res = pg_query($sql);
+
+  // echo pg_result_error($res);
+  // echo "\n";
+  // echo pg_result_status($res);
+  // echo "\n";
+  // echo pg_num_rows($res);
+  // echo "\n";
+  //echo pg_options($connection); 
+  
+
   if (!$res) {
     echo "Ein Fehler ist aufgetreten.\n";
     exit;
   }
   $arr_data = pg_fetch_all($res);
+  pg_free_result($res);
+  pg_close($connection);
   //print_r($arr_data);
   // echo is_array($arr_data) ? 'Array' : 'kein Array';
-  // echo "\n";
+  //echo "\n";
 
   $processed = array();
   
   foreach (($arr_data) as $key => $value) {
-    //print_r(json_decode($d['features'], TRUE));
+    //print_r(json_decode($value['features'], TRUE));
     //echo empty($value['features']) ? 'leeres Features' : 'FEATURE!';
     //echo nl2br("\n\n");
     $processed[$key] = json_decode($value['features'], TRUE);
@@ -39,5 +52,6 @@ if (isset($_GET['id'])) {
   // write json file to disc and echo
   //file_put_contents('reservoirsById.json', $json);
   echo $json;
+  
 }
 ?>
